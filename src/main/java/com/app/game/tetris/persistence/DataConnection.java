521309fba9ed087ef6e865332dec095b69abee21
Andrei Tarassov <tamogenia@gmail.com>
2024-05-06 10:29:59 +0300

    Added DB credentials to environment

--- src/main/java/com/app/game/tetris/persistence/DataConnection.java
+++ src/main/java/com/app/game/tetris/persistence/DataConnection.java
@@ -2,27 +2,17 @@ package com.app.game.tetris.persistence;
 
 import org.springframework.stereotype.Component;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 @Component
 public class DataConnection {
     private Connection con;
 
     public Connection getConnection() throws SQLException {
-        String fileLocation = System.getProperty("user.dir") + "\\db_Credentials.properties";
-        Properties propertiesDB = new Properties();
-        try {
-            propertiesDB.load(new FileReader(fileLocation));
-            if (con == null)
-                con = DriverManager.getConnection(propertiesDB.getProperty("url"), propertiesDB.getProperty("user"), propertiesDB.getProperty("password"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        if (con == null)
+            con = DriverManager.getConnection(System.getenv("url"), System.getenv("user"), System.getenv("password"));
         return con;
     }
 }
