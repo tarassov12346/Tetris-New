445f47fb9617cd6c6555acdc70dd024508442b15
Andrei Tarassov <tamogenia@gmail.com>
2024-05-06 09:26:40 +0300

    moved DB credentials to properties file

--- src/main/java/com/app/game/tetris/persistence/DataConnection.java
+++ src/main/java/com/app/game/tetris/persistence/DataConnection.java
@@ -2,19 +2,27 @@ package com.app.game.tetris.persistence;
 
 import org.springframework.stereotype.Component;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 @Component
 public class DataConnection {
     private Connection con;
-    private String user = "postgres";
-    private String password = "mine";
-    private String url = "jdbc:postgresql:game";
 
     public Connection getConnection() throws SQLException {
-        if (con==null) con=DriverManager.getConnection(url, user, password);
+        String fileLocation = System.getProperty("user.dir") + "\\db_Credentials.properties";
+        Properties propertiesDB = new Properties();
+        try {
+            propertiesDB.load(new FileReader(fileLocation));
+            if (con == null)
+                con = DriverManager.getConnection(propertiesDB.getProperty("url"), propertiesDB.getProperty("user"), propertiesDB.getProperty("password"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return con;
     }
 }
